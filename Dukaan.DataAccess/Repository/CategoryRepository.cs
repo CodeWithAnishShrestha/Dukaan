41bb77f5b5679b26c3355f15da8e3b557a252691
CodeWithAnishShrestha <growwithanish@gmail.com>
2024-05-20 13:43:29 +0545

    Unit of work and services added to the container

---
+++ Dukaan.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dukaan.DataAccess.Data;
+using Dukaan.DataAccess.Repository.IRepository;
+using Dukaan.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukaan.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ Dukaan.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dukaan.DataAccess.Data;
+using Dukaan.DataAccess.Repository.IRepository;
+using Dukaan.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukaan.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
